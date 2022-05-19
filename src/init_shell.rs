53919253950dbde69bea64320b96a86ad48229d6
Andrew Gaspar <agaspar@fb.com>
2022-05-19 16:37:23 -0600

    shell integration

---
+++ src/init_shell.rs
@@ -0,0 +1,39 @@
+use quick_error::quick_error;
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum Error {
+        ShellNotSupported
+        Io(err: std::io::Error) {
+            from()
+        }
+    }
+}
+
+fn bash_shell() -> Result<(), Error> {
+    let script = format!(
+        r#"
+xadb () {{
+    export XADB_INIT_SHELL=bash
+    export XADB_TEMP_FILE=$(mktemp /tmp/xadb-script.XXXXXX)
+    {} $@
+    source "${{XADB_TEMP_FILE}}"
+    rm "${{XADB_TEMP_FILE}}"
+    unset XADB_TEMP_FILE
+    unset XADB_INIT_SHELL
+}}
+    "#,
+        std::env::current_exe()?.to_str().unwrap()
+    );
+
+    println!("{script}");
+
+    Ok(())
+}
+
+pub fn init_shell(shell: &str) -> Result<(), Error> {
+    match shell {
+        "bash" => bash_shell(),
+        _ => Err(Error::ShellNotSupported),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use device_select::DeviceSelectApp;
 use tui::{backend::CrosstermBackend, Terminal};
 
 mod cache;
+mod init_shell;
 mod commands {
     pub(crate) mod adb;
     pub(crate) mod fastboot;
@@ -32,12 +33,16 @@ struct Args {
 
 #[derive(Subcommand)]
 enum Command {
-    #[clap(about = "Interactive selection for adb device")]
-    Select,
+    #[clap(about = "Interactive list of adb devices")]
+    List,
     #[clap(about = "Clear xadb cache")]
     ClearCache,
     #[clap(about = "Get product for currently selected adb device")]
     CurrentProduct,
+    #[clap(about = "Print shell integration function")]
+    InitShell { shell: String },
+    #[clap(about = "Interactively select adb device to use in current shell")]
+    Select,
 }
 
 async fn build_and_run_app(
@@ -54,7 +59,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     match args.command {
-        Command::Select => {
+        Command::List => {
             // setup terminal
             enable_raw_mode()?;
             let mut stderr = io::stderr();
@@ -110,5 +115,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             Ok(())
         }
+        Command::InitShell { shell } => Ok(init_shell::init_shell(&shell)?),
+        Command::Select => match std::env::var("XADB_INIT_SHELL") {
+            Ok(var) => {
+                assert_eq!("bash", var, "Only bash is currently supported");
+
+                let var = std::env::var("XADB_TEMP_FILE").expect("XADB_TEMP_FILE not set!");
+                tokio::fs::write(
+                    var,
+                    format!(
+                        r#"
+export ANDROID_SERIAL=$({} list)
+                "#,
+                        std::env::current_exe().unwrap().to_str().unwrap(),
+                    ),
+                )
+                .await?;
+                Ok(())
+            }
+            Err(_) => {
+                eprintln!(
+                    r#"This shell has not be initialized. Place the following in your .bashrc:
+eval "$(xadb init-shell bash)"
+                    "#
+                );
+                std::process::exit(1);
+            }
+        },
     }
 }
